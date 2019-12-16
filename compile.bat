4d397db1a03f6e507f79f15ca14340da3ab05aed
NonstickAtom785 <36454623+NonstickAtom785@users.noreply.github.com>
2019-12-15 20:27:30 -0600

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -14,6 +14,6 @@ python jt.py jmptable.z80 grammer2.5.inc
 copy /Y grammer2.5.inc ..\docs\grammer2.5.inc
 
 echo "Assembling App"
-..\spasm.exe grammer.z80 ..\bin\remmag.8xk -I single
+..\spasm.exe grammer.z80 ..\bin\remmag.8xk
 
 cd..
