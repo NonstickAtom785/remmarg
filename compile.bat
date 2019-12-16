217960668ae5d81640faea9a0f6765a90d34b191
NonstickAtom785 <36454623+NonstickAtom785@users.noreply.github.com>
2019-12-15 20:27:10 -0600

    Change compile destination

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ if exist spasm.exe (
   echo "Starting Compile..."
 ) else (
   echo "Opening readme..."
-  start "" https://github.com/Zeda/Grammer2/blob/master/Readme.md
+  start "" https://github.com/Zeda/remmarg
   exit
 )
 
@@ -13,16 +13,7 @@ echo "Generating grammer2.5.inc"
 python jt.py jmptable.z80 grammer2.5.inc
 copy /Y grammer2.5.inc ..\docs\grammer2.5.inc
 
-echo "Assembling Default Package"
-..\spasm.exe grampkg.z80 ..\bin\grampkg.8xv
-
 echo "Assembling App"
-..\spasm.exe grammer.z80 ..\bin\grammer.8xk -I single
-
-rem Check for an experimental package
-if exist experimental\experimental.z80 (
-  echo "Assembling Experimental Package"
-  ..\spasm.exe experimental\experimental.z80 ..\bin\EXPRMNTL.8xv -I single
-)
+..\spasm.exe grammer.z80 ..\bin\remmag.8xk -I single
 
 cd..
